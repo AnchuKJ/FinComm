3a900fa019513f618bbcfc0d2c1fbf6f986fbfc1
preethimickle <preethimickle423@gmail.com>
2022-03-21 21:30:42 +0530

    UI Changes
    UI changes

--- FinComm-UI/fincomm/src/app/app.module.ts
+++ FinComm-UI/fincomm/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CommunitydetailsComponent } from './communitydetails/communitydetails.c
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatInputModule} from '@angular/material/input';
 
+
 @NgModule({
   declarations: [
     AppComponent,
