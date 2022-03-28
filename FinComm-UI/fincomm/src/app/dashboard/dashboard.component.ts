40631de39b2de2425322ac97917ae6b8d80f220c
preethimickle <preethimickle423@gmail.com>
2022-03-28 23:04:15 +0530

    dashboard

--- FinComm-UI/fincomm/src/app/dashboard/dashboard.component.ts
+++ FinComm-UI/fincomm/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,15 +19,37 @@ export class DashboardComponent implements OnInit {
  ];
  communitytotal=[
   { name:"Loan", value:5000},
-  {name:"Savings", value:12000}
+  {name:"Savings", value:15000}
 ];
 communitychartcolor=[
   { name: "Loan", value: '#8E30FF' },
   { name: "Savings", value: '#CF7CE1' },
 ];
-legendTitle="Savings Rate";
-  constructor() { }
 
+legendTitle="Savings Rate";
+incomechartcolor=[
+  {name:"Oct 2021",value:'#8E30FF'},
+  {name:"Nov 2021",value:'#8E30FF'},
+  {name:"Dec 2021",value:'#8E30FF'},
+  {name:"Jan 2021",value:'#8E30FF'},
+  {name:"Feb 2021",value:'#8E30FF'}
+];
+incomeData = [
+  { name: "Oct 2021", value: 105000 },
+  { name: "Nov 2021", value: 55000 },
+  { name: "Dec 2021", value: 15000 },
+  { name: "Jan 2021", value: 150000 },
+  { name: "Feb 2021", value: 20000 }
+];
+  constructor(public router:Router) {
+  
+   }
+
+  public gotoaddcommunity()
+  {
+    this.router.navigate(['/addcommunity']);
+  }
+  
   ngOnInit(): void {
   }
 
